f6453bf2288ea253eb22958b3dd2e9b2c6d9830c
Anjana Nair <163915728+4Anj@users.noreply.github.com>
2026-07-14 01:28:25 +0530

    Format allow_origins for better readability

--- backend/ev_api.py
+++ backend/ev_api.py
@@ -79,7 +79,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000","https://ev-monitoring-frontend.onrender.com",]
+    allow_origins=[
+        "http://localhost:3000",
+        "https://ev-monitoring-frontend.onrender.com",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
